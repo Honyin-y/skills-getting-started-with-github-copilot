d0cbce359cbbdcfdb796aa3160ac1b426d70e9e6
Honyin-y <tintinyeh2014@gmail.com>
2026-03-13 02:13:09 +0000

    Add 2 more sports related activities, 2 more artistic activities, and 2 more intellectual activities.

--- src/app.py
+++ src/app.py
@@ -66,6 +66,7 @@ def signup_for_activity(activity_name: str, email: str):
         raise HTTPException(status_code=400, detail="Student already signed up for this activity")
 
 
+
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
